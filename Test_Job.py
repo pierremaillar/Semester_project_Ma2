76ea65e055326e08fc6c5bc8513474ad99430ebc
pierremaillar <pierre.maillard@epfl.ch>
2024-04-04 20:55:09 +0200

    JobTest

---
+++ Test_Job.py
@@ -0,0 +1,66 @@
+# Useful starting lines
+import numpy as np
+import pandas as pd
+import time
+
+from create_batch import *
+from datatreatment import *
+from neuralnet import *
+from visualisations import *
+
+
+
+
+path_to_dataset ="dataset_hsp70_tax\dataset_hsp70_tax.csv" 
+hsp70 = importing_data(path_to_dataset)
+
+
+level3, level3_categ= get_data(hsp70, 3, 6,"Bacteria",Use_Others=False)
+level3=encode01(level3)
+level3=category_to_int(level3,level3_categ)
+
+
+
+best_params_nn = {'layer_dim': 64, 'number_hidden_layer': 3, 'dropout_prob': 0.2, 'l2_regu': 1e-05, 'weight_decay': 0.0001, 'learning_rate': 0.001, 'batch_size': 128, 'num_epochs': 10}
+
+layer_dim = best_params_nn['layer_dim']
+number_hidden_layer = best_params_nn['number_hidden_layer']
+dropout_prob = best_params_nn['dropout_prob']
+l2_regu = best_params_nn['l2_regu']
+weight_decay = best_params_nn['weight_decay']
+learning_rate = best_params_nn['learning_rate']
+batch_size = best_params_nn['batch_size']
+num_epochs = best_params_nn['num_epochs']
+
+
+positions_to_keep =range(1,600)
+output_dim = 6
+tic = time.time()
+dfs = []
+
+for i in range(30):
+    
+    train, train_label, test, test_label, val, val_label=split_dataset(level3, 0.8, 0.1, 0.1)
+    input_dim = train.shape[1]
+    
+    model_neural = ModelClassification(input_dim, output_dim, layer_dim, number_hidden_layer, dropout_prob, l2_regu)
+    optimizer = torch.optim.Adam(model_neural.parameters(), lr = learning_rate, weight_decay=weight_decay)#lr : learning rate
+    train_model(model_neural, num_epochs, train, train_label, test, test_label, optimizer, batch_size)
+    
+    dfs.append(feature_importances_neural(model_neural, level3, smoothness = 0, pos =positions_to_keep, plot = 0))
+    t = time.time() - tic
+    print(f"Got to iteration {i+1} in {t} seconds")
+    
+    
+
+arrays = [df.to_numpy() for df in dfs]
+
+stacked_array = np.stack(arrays, axis=0)
+mean_values = np.mean(stacked_array, axis=0)
+std_values = np.std(stacked_array, axis=0)
+
+
+np.savetxt('mean.txt', mean_values)
+np.savetxt('std.txt', std_values)
+
+print("Job finished")
