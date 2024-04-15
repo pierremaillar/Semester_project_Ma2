39d059668175d936e7483c31859d3ec67039ce36
pierremaillar <pierre.maillard@epfl.ch>
2024-04-15 09:53:48 +0200

    Update Job

--- Interp_opti.py
+++ Interp_opti.py
@@ -23,9 +23,9 @@ columns_info = level3.drop(level3.columns[0], axis=1).columns
 
 
 param_grid = {
-        'layer_dim': [64,96,128,192,256],
+        'layer_dim': [64,96,128,192],
         'number_hidden_layer': [2,3,4],
-        'dropout_prob': [0.2,0.3,0.4,0.5,0.6,0.7],
+        'dropout_prob': [0.3,0.4,0.6,0.7],
         'l2_regu': [1e-05],
         'weight_decay': [0.0001],
         'learning_rate':[0.0001],
