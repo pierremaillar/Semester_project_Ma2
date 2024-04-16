5d8f2d2f5516b98758af056ebfd77fcd4ac215db
pierremaillar <pierre.maillard@epfl.ch>
2024-04-16 10:24:12 +0200

    Update Job

--- Interp_opti_BEA.py
+++ Interp_opti_BEA.py
@@ -15,7 +15,7 @@ path_to_dataset ="dataset_hsp70_tax/dataset_hsp70_tax.csv"
 hsp70 = importing_data(path_to_dataset)
 
 
-level3, level3_categ= get_data(hsp70, 2, 3,"Cellular Organism",Use_Others=False)
+level3, level3_categ= get_data(hsp70, 2, 3,Use_Others=False)
 level3=encode01(level3)
 level3=category_to_int(level3,level3_categ)
 columns_info = level3.drop(level3.columns[0], axis=1).columns
@@ -37,7 +37,7 @@ nbr_training = 10
 
 train, train_label, test, test_label, val, val_label=split_dataset(level3, 0.8, 0.1, 0.1)
 
-output_dim = 6
+output_dim = 3
 best_params_nn, mean_values, std_values = optimize_hyperparameters_nn_score(train, train_label, val, val_label, output_dim,columns_info,param_grid,nbr_training,positions_to_keep)
 
 
