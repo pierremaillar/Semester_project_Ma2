8fa7a59b9e991c177bb2e0cd2bf723024f5f9b63
pierremaillar <pierre.maillard@epfl.ch>
2024-04-16 09:53:42 +0200

    Update Job

--- Interp_opti_BEA.py
+++ Interp_opti_BEA.py
@@ -20,15 +20,14 @@ level3=encode01(level3)
 level3=category_to_int(level3,level3_categ)
 columns_info = level3.drop(level3.columns[0], axis=1).columns
 
-
 param_grid = {
-        'layer_dim': [32,64],
-        'number_hidden_layer': [2],
-        'dropout_prob': [0.4],
+        'layer_dim': [32,64,128],
+        'number_hidden_layer': [2,3,4],
+        'dropout_prob': [0.4,0.6,0.8],
         'l2_regu': [1e-05],
         'weight_decay': [0.0001],
         'learning_rate':[0.0001],
-        'batch_size':[32],
+        'batch_size':[32,64,128],
         'num_epochs':[10]
         }
 
