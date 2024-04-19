95a3e4c44760280c2b3066fae01b9587546c3831
pierremaillar <pierre.maillard@epfl.ch>
2024-04-19 14:47:19 +0200

    Add pos score job

--- Job_pos_scores.py
+++ Job_pos_scores.py
@@ -7,8 +7,12 @@ from neuralnet import *
 from visualisations import *
 
 
+level3, level3_categ= get_data(hsp70, 3, 6,"Bacteria",Use_Others=False)
+level3=encode01(level3)
+level3=category_to_int(level3,level3_categ)
 
-best_params_nn = {'layer_dim': 64, 'number_hidden_layer': 3, 'dropout_prob': 0.2, 'l2_regu': 1e-05, 'weight_decay': 0.0001, 'learning_rate': 0.001, 'batch_size': 128, 'num_epochs': 10}
+
+best_params_nn = {'layer_dim': 64, 'number_hidden_layer': 4, 'dropout_prob': 0.6, 'l2_regu': 1e-05, 'weight_decay': 0.0001, 'learning_rate': 0.001, 'batch_size': 64, 'num_epochs': 10}
 
 
 layer_dim = best_params_nn['layer_dim']
@@ -46,6 +50,25 @@ arrays = [df.to_numpy() for df in dfs]
 stacked_array = np.stack(arrays, axis=0)
 mean_values = np.mean(stacked_array, axis=0)
 std_values = np.std(stacked_array, axis=0)
+
+
+
+
+plt.figure(figsize=(50, 6))
+plt.errorbar(positions_to_keep, mean_values[:, 0], std_values[:, 0], capsize=4, color="red", fmt="o", markersize=4)
+
+plt.xlabel('Positions')
+plt.ylabel('Scores')
+plt.title('Position importances')
+
+
+num_ticks = len(positions_to_keep)//15
+xticks_indices = np.linspace(0, len(positions_to_keep) - 1, num_ticks, dtype=int)
+plt.xticks(np.array(positions_to_keep)[xticks_indices])
+
+plt.savefig('Postitionimportances.png', dpi=200)
+
+
 
 
 np.savetxt('mean.txt', mean_values)
